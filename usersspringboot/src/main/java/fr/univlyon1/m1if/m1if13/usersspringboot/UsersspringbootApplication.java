3a3c7b82c9b37a2c51bec6fe786600713d49ad75
BMW <mohamedwassim.beldjillali@gmail.com>
2020-02-12 10:55:39 +0100

    Mise en place

---
+++ usersspringboot/src/main/java/fr/univlyon1/m1if/m1if13/usersspringboot/UsersspringbootApplication.java
@@ -0,0 +1,13 @@
+package fr.univlyon1.m1if.m1if13.usersspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersspringbootApplication.class, args);
+	}
+
+}
---
+++ usersspringboot/src/test/java/fr/univlyon1/m1if/m1if13/usersspringboot/UsersspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.univlyon1.m1if.m1if13.usersspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersspringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
